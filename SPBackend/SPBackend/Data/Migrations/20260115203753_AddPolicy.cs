61e390fb4ee48319d05c00a7b59715ff7b0ee36f
elie-sa <e.sawmaawad@gmail.com>
2026-01-18 10:17:48 +0100

    Add migrations

---
+++ SPBackend/SPBackend/Data/Migrations/20260115203753_AddPolicy.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SPBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPolicy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Policies_PolicyTypes_PolicyTypeId",
+                table: "Policies");
+
+            migrationBuilder.DropTable(
+                name: "PolicyTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Policies_PolicyTypeId",
+                table: "Policies");
+
+            migrationBuilder.DropColumn(
+                name: "GreaterThan",
+                table: "Policies");
+
+            migrationBuilder.DropColumn(
+                name: "LessThan",
+                table: "Policies");
+
+            migrationBuilder.DropColumn(
+                name: "PolicyTypeId",
+                table: "Policies");
+
+            migrationBuilder.AddColumn<long>(
+                name: "PowerSourceId",
+                table: "Policies",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "TempGreaterThan",
+                table: "Policies",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "TempLessThan",
+                table: "Policies",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Policies_PowerSourceId",
+                table: "Policies",
+                column: "PowerSourceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Policies_PowerSources_PowerSourceId",
+                table: "Policies",
+                column: "PowerSourceId",
+                principalTable: "PowerSources",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Policies_PowerSources_PowerSourceId",
+                table: "Policies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Policies_PowerSourceId",
+                table: "Policies");
+
+            migrationBuilder.DropColumn(
+                name: "PowerSourceId",
+                table: "Policies");
+
+            migrationBuilder.DropColumn(
+                name: "TempGreaterThan",
+                table: "Policies");
+
+            migrationBuilder.DropColumn(
+                name: "TempLessThan",
+                table: "Policies");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "GreaterThan",
+                table: "Policies",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LessThan",
+                table: "Policies",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "PolicyTypeId",
+                table: "Policies",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "PolicyTypes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PolicyTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Policies_PolicyTypeId",
+                table: "Policies",
+                column: "PolicyTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Policies_PolicyTypes_PolicyTypeId",
+                table: "Policies",
+                column: "PolicyTypeId",
+                principalTable: "PolicyTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ SPBackend/SPBackend/Data/Migrations/20260116230754_AddScheduleStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SPBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddScheduleStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Schedules",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Schedules");
+        }
+    }
+}
