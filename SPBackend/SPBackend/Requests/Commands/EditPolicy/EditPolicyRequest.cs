d52414b4f26fd4208b74944f1fc19c37f62ea189
elie-sa <e.sawmaawad@gmail.com>
2026-02-09 16:58:49 +0200

    Fix edit policy non null args

--- SPBackend/SPBackend/Requests/Commands/EditPolicy/EditPolicyRequest.cs
+++ SPBackend/SPBackend/Requests/Commands/EditPolicy/EditPolicyRequest.cs
@@ -7,9 +7,9 @@ public class EditPolicyRequest: IRequest<EditPolicyResponse>
 {
     public long Id { get; set; }
     public string Name { get; set; }
-    public long PowerSourceId { get; set; }
-    public double TempGreaterThan { get; set; }
-    public double TempLessThan { get; set; }
+    public long? PowerSourceId { get; set; }
+    public double? TempGreaterThan { get; set; }
+    public double? TempLessThan { get; set; }
     public List<long> OnPlugIds { get; set; } = new();
     public List<long> OffPlugIds { get; set; } = new();
 }
